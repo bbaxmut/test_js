b8834d5ff02c5b232a0a0a697ed7aff57ff59b24
bbaxmut <contentdiller@gmail.com>
2021-10-09 01:31:48 +0300

    DZ_3

---
+++ DZ_3.js
@@ -0,0 +1,70 @@
+
+// let square = function (number) {
+//     return number * number;
+// };
+// let result = square(9);
+// console.log(result);
+
+// let resultat = function (num1, num2, num3) {
+//     return ((num1 - num2) / num3);
+// };
+// let resultat1 = resultat(10,4,3);
+// console.log(resultat1);
+
+
+// let day = function ();
+
+
+//Задача №1
+// let arr = ['Ваня', 'Иштван', 'Оля'];
+// console.log(arr.length);
+// let newArr = arr;
+// newArr.push('Петя');
+// console.log(arr.length);
+
+//Задача №2
+// let users = ['Ваня', 'Иштван'];
+// users.push('Оля');
+// console.log(users);
+// users.splice(1,1,'Петя');
+// console.log(users);
+// users.shift([0]);
+// console.log(users);
+// users.unshift('Маша', 'Паша');
+// console.log(users);
+
+// let str = 'Ваня, Иштван, Оля';
+// let arr = str.split(',');
+// console.log(arr);
+
+// let arr = [1, 2, 3, 4, 5];
+// let newArr = arr.map(number => {
+//     return number * number
+//     }
+// );
+// console.log(newArr);
+
+// let arr = [1, 2, 3, 4, 5];
+// let newArr = [];
+// arr.forEach(function (number) {
+//     newArr.push(number * number);
+// });
+// console.log(newArr);
+
+
+// let arr = [1, 2, 3, 4, 5];
+// let newArr = [];
+// arr.forEach(function (number) {
+//     newArr.push(number * number);
+// });
+// console.log(newArr);
+// let sum = 0;
+// function sum(newArr) {
+//     let i, sum = 1;
+//    for (i = 0; i < newArr.length; i++)
+//        sum += newArr[i];
+//    return sum;
+//    }
+//
+// console.log(sum());
+//
---
+++ array.js
@@ -0,0 +1,49 @@
+// let arr3 = [1, 2 , 3];
+// arr3.map(item * 2);
+// console.log(arr3.map);
+
+
+// let arr1 = ['Яблоко', 'Груша'];
+// arr1.forEach(function (item,index) {
+//     console.log(item, index);
+// });
+
+
+// let fruits = ['Яблоко ', 'Банан', 'Слива'];
+// fruits.push('Манго');
+// console.log(fruits.shift() );
+// console.log(fruits);
+// console.log(fruits.length);
+// fruits.unshift('Абрикос');
+// console.log(fruits);
+
+
+// let fruits = ['Яблоко ', 'Банан', 'Слива'];
+// for (let key in fruits) {
+//     console.log(fruits);
+// };
+
+// let fruits = ['Яблоко ', 'Банан', 'Слива','Манго',];
+// fruits.length = [3];
+// console.log(fruits);
+// fruits.length = [5];
+// console.log(fruits);
+// fruits.length = [0];
+// console.log(fruits);
+
+
+// let matrix = [
+//     [1, 2 , 3],
+//     [4, 5 , 6],
+//     [7, 8, 9]
+// ];
+// console.log(matrix[1][2]);
+
+// let people = [
+//     {name: 'Анастасия', age: 27, cash: 8000 },
+//     {name: 'Богдан', age: 24, cash: 28000},
+//     {name: 'Дмитрий', age: 25, cash: 48000},
+//     {name: 'Руслана', age: 19, cash: 100000}
+// ]
+// people.forEach(person => console.log(person));
+//
