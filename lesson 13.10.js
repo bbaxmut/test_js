1435feff6c3ab21238718da12688a406bafd4600
bbaxmut <contentdiller@gmail.com>
2021-10-26 00:52:05 +0300

    DOM

--- lesson 13.10.js
+++ lesson 13.10.js
@@ -1,54 +1,64 @@
-// let arr = ['Odin', 'Dva', 'tri'];
-// let newArr = arr.filter((item) => {
-//     return item[0].toUpperCase() === item[0];
-// });
-// console.log(newArr);
+// const heading = document.querySelectorAll('.fff');
+// console.log(heading);
 
-//  Объекты
+// const idList = document.querySelector('#list_1');
+// console.log(idList);
+ const heading_1 = document.querySelector('.heading_1');
+const heading_2 = document.querySelector('.heading_2');
+const heading_3 = document.querySelector('.heading_3')
 
+setTimeout ( () =>{
+ addStyleSTo(heading_1, 'Javascript','red', 'black', '3rem')
+}, 1500);
 
+setTimeout ( () =>{
+ addStyleSTo(heading_2, 'Практика','white', 'blue', '3rem')
+}, 3000);
 
-// let user = {
-//     name: 'Богдан',
-//     surname: 'Бахмут',
-//     age: 24,
-//     cash: 20000,
-//     city: 'Kharkiv',
-// };
-// const {name, surname, age} = user;
-// console.log(name, surname, age);
+setTimeout ( () => {
+ addStyleSTo(heading_3.querySelector('a'),'blue', 'red', '3rem')
+}, 4500);
 
-// const keyAge = 'age';
-// console.log(user[keyAge]);
+function addStyleSTo (node,text, color,backgroundColor, fontSize ) {
+ node.textContent = text
+ node.style.color = color
+ node.style.backgroundColor = backgroundColor
+ node.style.textAlign = 'center'
+ node.style.fontSize = fontSize
+ node.style.display = 'block'
+ node.style.width = '100%'
+}
 
-// delete user['surname'];
-// console.log(user);
-
-// for (let key in user) {
-//     console.log('key-',key);
-//     console.log('value-',user[key]);
-// }
-//
-// const keys = Object.keys(user);
-// keys.forEach( (key) => {
-//     console.log('key:',key);
-// console.log('value:',user[key]);
-// });
-
-// console.log('bla' in user);
-
-// let user = {};
-// user['name'] = 'John';
-// user['surname'] = 'Smith';
-// user['name'] = 'Pete';
-// delete user.name;
-// console.log(user);
+heading_1.onclick = () => {
+ if (heading_1.style.color === 'red') {
+  heading_1.style.color = 'blue'
+  heading_1.style.backgroundColor = 'yellow'
+ }
+ else {
+  heading_1.style.color = 'yellow'
+  heading_1.style.backgroundColor = 'green'
+ }
+}
 
 
-// let schedule = {};
-// function eisEmpty(schedule) {
-//     for (let key in schedule) {
-//         console.log('false');
-//     };
-// console.log('true');
-// };
+heading_2.ondblclick = () => {
+ if (heading_2.style.color === 'white') {
+  heading_2.style.color = 'orange'
+  heading_2.style.backgroundColor = 'grey'
+ }
+ else {
+  heading_2.style.color = 'yellow'
+  heading_2.style.backgroundColor = 'green'
+ }
+}
+
+heading_3.addEventListener('mouseup', () => {
+ if (heading_2.style.color === 'red') {
+  heading_2.style.color = 'white'
+  heading_2.style.backgroundColor = 'grey'
+ }
+ else {
+  heading_2.style.color = 'yellow'
+  heading_2.style.backgroundColor = 'black'
+ }
+})
